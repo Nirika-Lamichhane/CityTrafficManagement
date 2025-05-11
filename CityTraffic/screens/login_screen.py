a7fe6e6f854bd278e8f7d4b47191062db1567ca4
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-05-11 09:21:11 +0545

    login dashboard change

--- CityTraffic/screens/login_screen.py
+++ CityTraffic/screens/login_screen.py
@@ -1,3 +1,4 @@
+'''
 from kivy.uix.screenmanager import Screen
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
@@ -26,6 +27,23 @@ class LoginScreen(Screen):
         self.manager.current = "dashboard"  # Switch to the dashboard screen
 
 '''
+'''
 kwargs is used to pass the numbers of different types of variables to the function
 
 '''
+
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+Builder.load_file("screens/logic_screen.kv")
+# The above line loads the kv file for the login screen
+
+class LoginScreen(Screen):
+    def login(self):
+        username = self.ids.username.text
+        password = self.ids.password.text
+        if username == "admin" and password == "123":
+            print("Login successful!")
+            self.manager.current = "dashboard"
+        else:
+            print("Invalid credentials")
