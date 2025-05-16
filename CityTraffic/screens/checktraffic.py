e5fb36a26d8687b1eae1f43d4c8c3dff55af3727
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-05-16 23:11:14 +0545

    made changes

--- CityTraffic/screens/checktraffic.py
+++ CityTraffic/screens/checktraffic.py
@@ -6,14 +6,16 @@ Builder.load_file("screens/checktraffic.kv")
 
 
 class CheckTrafficScreen(Screen):
-    def check_traffic(self):
-        origin=self.ids.origin_input.text
-        destination=self.ids.destination_input.text
+   def check_traffic(self):
+    origin = self.ids.origin_spinner.text
+    destination = self.ids.destination_spinner.text
 
-        if origin and destination:
-            result=get_traffic_status(origin, destination)
-            self.ids.content_label.text=f"Traffic from {result['origin']} to {result['destination']} is {result['traffic_level']}"
-            self.ids.origin_input.text=""
-            self.ids.destination_input.text=""
-        else:
-            self.ids.content_label.text="Please enter both origin and destination."
+    if origin != "Select Origin" and destination != "Select Destination":
+        result = get_traffic_status(origin, destination)
+        self.ids.content_label.text = (
+            f"Traffic from {result['origin']} to {result['destination']} is {result['traffic_level']}"
+        )
+        self.ids.origin_spinner.text = "Select Origin"
+        self.ids.destination_spinner.text = "Select Destination"
+    else:
+        self.ids.content_label.text = "Please select both origin and destination."
